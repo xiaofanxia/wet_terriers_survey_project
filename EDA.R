1110209efc0d248d7cf340d4473c6dc0c6472630
JingningYang <54416899+JingningYang@users.noreply.github.com>
2019-10-30 00:57:14 -0400

    Add files via upload

--- EDA.R
+++ EDA.R
@@ -1,3 +1,16 @@
+library(lubridate)
+library(hms)
 survey <- read.csv("survey.csv")
-#survey$Start.Date <- gsub("^\\d ", "",survey$Start.Date)
-survey$Start.Date <- as.numeric(as.POSIXct(survey$Start.Date)) %% 86400
+survey$Start.Date <- gsub("[0-9]*/[0-9]*/\\d* ","", survey$Start.Date)
+survey$Start.Date <- gsub("[A-Z]", "",survey$Start.Date)
+survey$Start.Date <- gsub(":", ".",survey$Start.Date)
+survey$Start.Date <- gsub("[0-9]*-[0-9]*-\\d* ", "",survey$Start.Date)
+
+
+survey$End.Date <- gsub("[0-9]*/[0-9]*/\\d* ", "",survey$End.Date)
+survey$End.Date <- gsub("[A-Z]", "",survey$End.Date)
+survey$End.Date <- gsub(":", ".",survey$End.Date)
+survey$End.Date <- gsub("[0-9]*-[0-9]*-\\d* ", "",survey$End.Date)
+dif <- suppressWarnings(as.numeric(survey$End.Date) - as.numeric(survey$Start.Date))
+
+difftime(survey$Start.Date, survey$End.Date)
