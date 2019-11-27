d4cfd306da492d9154883ebc982973348cd7b56c
Jingning Yang <jingningyang@Jingnings-MacBook-Pro.local>
2019-11-27 15:12:58 -0500

    jny

--- PF.R
+++ PF.R
@@ -27,7 +27,7 @@ personinfo <- aggregate[,1:5]
 surveyanswers <- aggregate[,-which(names(aggregate) %in% names(personinfo))]
 survey <- cbind(personinfo,surveyanswers)
 colnames(surveyanswers)[c(grep("PF1a _Confidence",colnames(surveyanswers)))] <- "PF1a_Confidence"
-colnames(surveyanswers)[c(grep("PF5c_Contributes",colnames(surveyanswers)))] <- "PF5c_Confidence"
+colnames(surveyanswers)[c(grep("PF5c_Contributes.1",colnames(surveyanswers)))] <- "PF5c_Confidence"
 
 data.PF <- surveyanswers[,c(grep("PF",colnames(surveyanswers)))]
 data.PF <- data.PF[,c(grep("Confidence",colnames(data.PF)))]
@@ -40,13 +40,12 @@ for (i in 1:ncol(data.PF)) {
 }
 colnames(PF.number) <- colnames(data.PF)
 
+
 # First subdomain
 m1 <- 'PF1 =~ PF1a_Confidence + PF1b_Confidence + PF1c_Confidence + PF1d_Confidence + PF1e_Confidence'
 fit1 <- cfa(m1,data = PF.number, std.lv=TRUE)
 #summary(fit1,fit.measures=TRUE,standardized=TRUE)
 summary(fit1,standardized=T)
-fitMeasures(fit1)
-modificationIndices(fit1)
 #P-value = 0.008
 parameterEstimates(fit1, standardized=TRUE) %>% 
   filter(op == "=~") %>% 
@@ -54,62 +53,78 @@ parameterEstimates(fit1, standardized=TRUE) %>%
   kable(digits = 3, format="pandoc", caption="Factor Loadings")
 #Factor loadings: 0.609; 0.830; 0.946; 0.915; 0.587
 
+#Since p-value of first subdomain is 0.008 < 0.05, and the factor loadings of "PF1e_Confidence" is lowest, thus, we try to drop it from the first subdomain
+m1.1 <- 'PF1 =~ PF1a_Confidence + PF1b_Confidence + PF1c_Confidence + PF1d_Confidence'
+fit1.1 <- cfa(m1.1,data = PF.number, std.lv=TRUE)
+summary(fit1.1,standardized=T)
+#P-value = 0.967 > 0.05, thus we do not need to change any more on the first subdomain.
+
+
 # Second subdomain
 m2 <- 'PF2 =~ PF2a_Confidence + PF2b_Confidence + PF2c_Confidence + PF2d_Confidence + PF2e_Confidence'
-fit2 <- cfa(m1,data = PF.number, std.lv=TRUE)
+fit2 <- cfa(m2,data = PF.number, std.lv=TRUE)
 summary(fit2,standardized=T)
-#P-value = 0.008
-fitMeasures(fit2)
-modificationIndices(fit2)
+#P-value = 0.013
 parameterEstimates(fit2, standardized=TRUE) %>% 
   filter(op == "=~") %>% 
   select('Latent Factor'=lhs, Indicator=rhs, B=est, SE=se, Z=z, 'p-value'=pvalue, loading=std.all) %>% 
   kable(digits = 3, format="pandoc", caption="Factor Loadings")
-#Factor loadings: 0.001; 0.000; 0.000; 0.000; 0.001
+#Factor loadings: 0.587; 0.905; 0.558; 0.901; 0.896
+
+#Since p-value of first subdomain is 0.013 < 0.05, and the factor loadings of "PF2c_Confidence" is lowest, thus, we try to drop it from the second subdomain
+m2.1 <- m2 <- 'PF2 =~ PF2a_Confidence + PF2b_Confidence + PF2d_Confidence + PF2e_Confidence'
+fit2.1 <- cfa(m2.1,data = PF.number, std.lv=TRUE)
+summary(fit2.1,standardized=T)
+#P-value = 0.459 > 0.05, thus we can stay here for the second subdomain.
+
 
 # Third subdomain
 m3 <- 'PF3 =~ PF3a_Confidence + PF3b_Confidence + PF3c_Confidence + PF3d_Confidence + PF3e_Confidence'
-fit3 <- cfa(m1,data = PF.number, std.lv=TRUE)
+fit3 <- cfa(m3,data = PF.number, std.lv=TRUE)
 #summary(fit1,fit.measures=TRUE,standardized=TRUE)
 summary(fit3,standardized=T)
-#P-value = 0.008
-fitMeasures(fit3)
-modificationIndices(fit3)
-
+#P-value = 0.712 
 parameterEstimates(fit3, standardized=TRUE) %>% 
   filter(op == "=~") %>% 
   select('Latent Factor'=lhs, Indicator=rhs, B=est, SE=se, Z=z, 'p-value'=pvalue, loading=std.all) %>% 
   kable(digits = 3, format="pandoc", caption="Factor Loadings")
-#Factor loadings: 0.609; 0.830; 0.946; 0.915; 0.587
+#Factor loadings: 0.485; 0.838; 0.746; 0.767; 0.588
+#Since p-value > 0.05, third subdomain is ok.
+
 
 # Fourth subdomain
 m4 <- 'PF4 =~ PF4a_Confidence + PF4b_Confidence
-PF5 =~ PF5a_Confidence + PF5b_Confidence + PF5c_Confidence'
-fit4 <- cfa(m1,data = PF.number, std.lv=TRUE)
+       PF5 =~ PF5a_Confidence + PF5b_Confidence + PF5c_Confidence'
+fit4 <- cfa(m4,data = PF.number, std.lv=TRUE)
 #summary(fit1,fit.measures=TRUE,standardized=TRUE)
 summary(fit4,standardized=T)
-#P-value = 0.008
-
+#P-value = 0.012 
 parameterEstimates(fit4, standardized=TRUE) %>% 
   filter(op == "=~") %>% 
   select('Latent Factor'=lhs, Indicator=rhs, B=est, SE=se, Z=z, 'p-value'=pvalue, loading=std.all) %>% 
   kable(digits = 3, format="pandoc", caption="Factor Loadings")
-#Factor loadings: 0.609; 0.83; 0.946; 0.915; 0.587
+#Factor loadings: 0.888; 0.835; 0.757; 0.759; 0.567
 
-# Fifth subdomain
-m5 <- 'PF5 =~ PF5a_Confidence + PF5b_Confidence + PF5c_Confidence'
-fit5 <- cfa(m1,data = PF.number, std.lv=TRUE)
-#summary(fit1,fit.measures=TRUE,standardized=TRUE)
-summary(fit5,standardized=T)
-#P-value = 0.008
+#Since P-value is 0.012 < 0.05, and the lowest factor loading is "PF5c_Confidence", thus we try to drop it from the subdomain
+m4.1 <- 'PF4 =~ PF4a_Confidence + PF4b_Confidence
+       PF5 =~ PF5a_Confidence + PF5b_Confidence'
+fit4.1 <- cfa(m4.1,data = PF.number, std.lv=TRUE)
+summary(fit4.1,standardized=T)
+#P-value is 0.362 > 0.05.
 
-parameterEstimates(fit5, standardized=TRUE) %>% 
-  filter(op == "=~") %>% 
-  select('Latent Factor'=lhs, Indicator=rhs, B=est, SE=se, Z=z, 'p-value'=pvalue, loading=std.all) %>% 
-  kable(digits = 3, format="pandoc", caption="Factor Loadings")
-#Factor loadings: 0.609; 0.83; 0.946; 0.915; 0.587
+# 
+# # Fifth subdomain
+# m5 <- 'PF5 =~ PF5a_Confidence + PF5b_Confidence + PF5c_Confidence'
+# fit5 <- cfa(m1,data = PF.number, std.lv=TRUE)
+# #summary(fit1,fit.measures=TRUE,standardized=TRUE)
+# summary(fit5,standardized=T)
+# #P-value = 0.008
+# 
+# parameterEstimates(fit5, standardized=TRUE) %>% 
+#   filter(op == "=~") %>% 
+#   select('Latent Factor'=lhs, Indicator=rhs, B=est, SE=se, Z=z, 'p-value'=pvalue, loading=std.all) %>% 
+#   kable(digits = 3, format="pandoc", caption="Factor Loadings")
+# #Factor loadings: 0.609; 0.83; 0.946; 0.915; 0.587
 
-
-#Check p-value, if p-value > 0.05, check loadings, if loadings > 0.6, keep it, then 
-
+#Check p-value, if p-value > 0.05, check loadings, if loadings > 0.6, keep it
 
